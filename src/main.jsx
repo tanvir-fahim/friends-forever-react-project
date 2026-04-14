da1783f862465af9da21cd70b4cfb2606c9baf0b
Tanvir-Fahim <mt009.fahim@gmail.com>
2026-04-14 17:25:29 +0600

    Tailwind, daisyUi, React router installed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <h2 className='bg-green-500 text-4xl p-10 text-center'>React Project Coming...</h2>
   </StrictMode>,
 )
