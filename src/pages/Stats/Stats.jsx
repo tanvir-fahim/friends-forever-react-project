a8b0c0b08af704a909b861c7920558920391d125
Tanvir-Fahim <mt009.fahim@gmail.com>
2026-04-17 01:32:51 +0600

    Sorting functionality added to Timeline

--- src/pages/Stats/Stats.jsx
+++ src/pages/Stats/Stats.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { FriendContext } from '../../context/FriendContext';
-import { MdOutlineDataUsage } from 'react-icons/md';
+import { FcStatistics } from 'react-icons/fc';
 import { Pie, PieChart, Cell, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Stats = () => {
@@ -47,7 +47,7 @@ const Stats = () => {
                                 ))}
                             </Pie>
 
-                            <Tooltip 
+                            <Tooltip
                                 contentStyle={{ borderRadius: '12px', border: 'none', boxShadow: '0 4px 6px -1px rgb(0 0 0 / 0.1)' }}
                             />
                             <Legend verticalAlign="bottom" height={36} iconType="circle" />
@@ -57,8 +57,8 @@ const Stats = () => {
             ) : (
                 <div className='container mx-auto px-10 mb-4'>
                     <div className="flex flex-col items-center justify-center min-h-75 border-2 border-dashed border-gray-200 rounded-2xl bg-gray-100/50 p-10">
-                        <div className="w-12 h-12 mb-4 rounded-full bg-white flex items-center justify-center shadow-sm">
-                            <span className="text-2xl text-gray-400"><MdOutlineDataUsage /></span>
+                        <div className="w-12 h-12  bg-gray-100/50 flex items-center justify-center">
+                            <span className="text-2xl"><FcStatistics /></span>
                         </div>
                         <h3 className="text-lg font-semibold text-gray-900">Nothing to show</h3>
                         <p className="text-gray-500 text-center max-w-3/4">
--- src/pages/Timeline/Timeline.jsx
+++ src/pages/Timeline/Timeline.jsx
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { FriendContext } from '../../context/FriendContext';
 import { MdAddIcCall } from 'react-icons/md';
 import { TiMessages } from 'react-icons/ti';
 import { IoVideocamOutline } from 'react-icons/io5';
 import { FaHistory } from 'react-icons/fa';
+import { IoMdArrowDropdown } from 'react-icons/io';
 
 
 
@@ -12,13 +13,30 @@ const Timeline = () => {
     const { timelineData } = useContext(FriendContext);
     // console.log(timelineData);
 
+    const [filter, setFilter] = useState('All');
+
+    const filteredTimelineData = timelineData?.filter(item => {
+        return filter === 'All' ? true : item.type === filter;
+    });
+
     return (
         <div className='container mx-auto'>
             <h2 className='text-3xl font-bold p-4'>Timeline</h2>
+            <div className="dropdown dropdown-bottom p-4">
+                <div tabIndex={0} role="button" className="btn m-1">
+                    {filter === 'All' ? 'Sort Timeline by' : `By ${filter}`} <IoMdArrowDropdown />
+                </div>
+                <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                    <li><a onClick={() => { setFilter('All'); document.activeElement.blur(); }}>All</a></li>
+                    <li><a onClick={() => { setFilter('Called'); document.activeElement.blur(); }}>Called</a></li>
+                    <li><a onClick={() => { setFilter('Texted'); document.activeElement.blur(); }}>Texted</a></li>
+                    <li><a onClick={() => { setFilter('Video Called'); document.activeElement.blur(); }}>Video Called</a></li>
+                </ul>
+            </div>
 
-            {timelineData && timelineData.length > 0 ? (
+            {filteredTimelineData && filteredTimelineData.length > 0 ? (
                 <div className="p-8 space-y-2">
-                    {timelineData.map((item, index) => (
+                    {filteredTimelineData.map((item, index) => (
                         <div
                             key={index}
                             className="flex items-center gap-4 p-4 border-2  border-blue-200 rounded-lg bg-white"
@@ -46,7 +64,7 @@ const Timeline = () => {
                     ))}
                 </div>
             ) : (
-                <div className='container mx-auto px-10 mb-4'> 
+                <div className='container mx-auto px-10 mb-4'>
                     <div className="flex flex-col items-center justify-center min-h-75 border-2 border-dashed border-gray-200 rounded-2xl bg-gray-100">
                         <div className="w-12 h-12 mb-4 rounded-full bg-gray-100 flex items-center justify-center">
                             <span className="text-2xl"><FaHistory /></span>
